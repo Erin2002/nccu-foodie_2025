56657cafbaf9cfedb54015467452ee514ab6e731
Erin2002 <121648603+Erin2002@users.noreply.github.com>
2025-06-04 21:15:19 +0800

    Update gameBoard.js

--- src/component/gameBoard.js
+++ src/component/gameBoard.js
@@ -85,7 +85,7 @@ export default function GameBoard() {
               padding: '12px 48px',
               marginTop: 24,
               borderRadius: 8,
-              background: '#4f8cff',
+              background: '#895129',
               color: '#fff',
               border: 'none',
               cursor: 'pointer',
